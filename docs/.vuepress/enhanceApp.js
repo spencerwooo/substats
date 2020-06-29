a28921aeb2bc80a6348ecaaf15c3b15fb171aecb
spencerwooo <spencerwoo98@foxmail.com>
2020-06-30 01:08:15 +0800

    Async import vue-toasted for SSR compatibility

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,15 @@
-import Toasted from 'vue-toasted'
 import Clipboard from 'v-clipboard'
 
 export default ({ Vue, options, router, siteData }) => {
-  Vue.use(Toasted, {
-    position: 'top-center',
-    duration: 3000,
-    keepOnHover: true,
-    theme: 'bubble',
-  })
   Vue.use(Clipboard)
+
+  // SSR compatible
+  import('vue-toasted').then(Toasted => {
+    Vue.use(Toasted.default, {
+      position: 'top-center',
+      duration: 3000,
+      keepOnHover: true,
+      theme: 'bubble',
+    })
+  })
 }
