26567edd02c5bc17b9f53e940551497ae8f4371a
spencerwooo <spencer.woo@outlook.com>
2022-02-19 23:13:08 +0800

    fix medium api response parsing

--- src/providers/medium.ts
+++ src/providers/medium.ts
@@ -2,6 +2,24 @@ import type { SubstatsResponse } from '@/types'
 import { commonProviderHandler } from '.'
 
 // https://medium.com/@spencerwooo?format=json
+type MediumRawResponse =
+  | {
+      success: true
+      payload: {
+        user: { userId: string }
+        references: {
+          SocialStats: Record<
+            string,
+            {
+              userId: number
+              usersFollowedCount: number
+              usersFollowedByCount: number
+            }
+          >
+        }
+      }
+    }
+  | { success: false; error: string }
 type MediumResponse =
   | { error: 0; followers: number }
   | { error: Omit<number, 0>; message: string }
@@ -11,18 +29,18 @@ const JSON_HIJACKING_PREFIX = '])}while(1);</x>'
 async function parseResponse(resp: Response): Promise<MediumResponse> {
   const text = await resp.text()
   const sanitiseText = text.replace(JSON_HIJACKING_PREFIX, '')
-  const data = JSON.parse(sanitiseText)
+  const data = JSON.parse(sanitiseText) as MediumRawResponse
 
   if (data.success) {
     // This required a middle processing procedure, where first we get the
     // userId to get a reference to the SocialStats object, and then we get the
     // followers count by the user's userId
     const uid = data.payload.user.userId
-    const user = data.payload.reference.SocialStats[uid]
+    const user = data.payload.references.SocialStats[uid]
     const followers = user.usersFollowedByCount
     return { error: 0, followers }
   } else {
-    return { error: 1, message: data?.error ?? 'Medium API request failed' }
+    return { error: 1, message: data.error }
   }
 }
 
