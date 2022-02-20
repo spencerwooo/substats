c89eb1e6b2ef5842adc6ccf48757af502960f419
spencerwooo <spencer.woo@outlook.com>
2022-02-20 17:30:27 +0800

    fix jike api response extraction

--- src/providers/jike.ts
+++ src/providers/jike.ts
@@ -5,20 +5,42 @@ import cheerio from 'cheerio'
 import { commonProviderHandler } from '.'
 
 // https://m.okjike.com/users/2204A477-38C8-4D9D-9705-9C9B990BE042
+type JikeRawResponse = {
+  props: {
+    pageProps: {
+      user: {
+        statsCount: {
+          topicSubscribed: number
+          topicCreated: number
+          followedCount: number
+          followingCount: number
+          highlightedPersonalUpdates: number
+          liked: number
+          respectedCount: number
+        }
+      }
+    }
+  }
+}
 type JikeResponse =
   | { error: 0; followers: number }
   | { error: 1; message: string }
 
 async function parseResponse(resp: Response): Promise<JikeResponse> {
   const html = await resp.text()
-  const $ = cheerio.load(html)
-  const raw = $('[type = "application/json"]').html()
 
-  if (raw) {
-    const cnt = JSON.parse(raw)?.props?.pageProps?.user?.followed_count ?? 0
-    return { error: 0, followers: cnt }
+  const extractPageProps = (): JikeRawResponse | null => {
+    const $ = cheerio.load(html)
+    const raw = $('[type = "application/json"]').html()
+    return raw ? (JSON.parse(raw) as JikeRawResponse) : null
   }
-  return { error: 1, message: 'No data found' }
+  const props = extractPageProps()
+  if (!props) {
+    return { error: 1, message: 'No data found' }
+  }
+
+  const followers = props.props.pageProps.user.statsCount.followedCount ?? 0
+  return { error: 0, followers }
 }
 
 export default async function jikeProvider(
@@ -28,7 +50,10 @@ export default async function jikeProvider(
     providerName: 'jike',
     queryKey: key,
     fetchUrl: `https://m.okjike.com/users/${key}`,
-    optionalHeaders: { Referer: `https://m.okjike.com/users/${key}` },
+    optionalHeaders: {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0',
+    },
     countObjPath: 'followers',
     errorMessageObjPath: 'message',
     isResponseValid: d => d.error === 0 && 'followers' in d,
