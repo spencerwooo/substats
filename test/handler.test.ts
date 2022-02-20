e011132fa69749b7a944e9e570cd270d44ffefda
spencerwooo <spencer.woo@outlook.com>
2022-02-20 13:34:24 +0800

    fix medium and reddit api

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -2,12 +2,16 @@ import { handleRequest } from '../src/handler'
 
 describe('handle', () => {
   test('handle GET', async () => {
-    const result = await handleRequest(new Request('http://localhost', { method: 'GET' }))
+    const result = await handleRequest(
+      new Request('http://localhost', { method: 'GET' }),
+    )
     expect(result.status).toEqual(200)
   })
 
   test('handle OPTIONS', async () => {
-    const result = await handleRequest(new Request('http://localhost', { method: 'OPTIONS' }))
+    const result = await handleRequest(
+      new Request('http://localhost', { method: 'OPTIONS' }),
+    )
     expect(result.status).toEqual(200)
     expect(result.headers.get('access-control-allow-methods')).toEqual(
       'GET, OPTIONS',
