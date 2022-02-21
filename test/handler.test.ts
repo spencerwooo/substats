67867f7754948370f3e005b155f9b2d2d38240a4
spencerwooo <spencer.woo@outlook.com>
2022-02-21 16:56:44 +0800

    remove newsblur and add test bindings

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -1,20 +1,22 @@
 import { handleRequest } from '../src/handler'
 
-describe('handle', () => {
-  test('handle GET', async () => {
-    const result = await handleRequest(
-      new Request('http://localhost', { method: 'GET' }),
-    )
-    expect(result.status).toEqual(200)
-  })
+test('handle GET', async () => {
+  const env = getMiniflareBindings()
+  const result = await handleRequest(
+    new Request('http://localhost', { method: 'GET' }),
+    env,
+  )
+  expect(result.status).toEqual(200)
+})
 
-  test('handle OPTIONS', async () => {
-    const result = await handleRequest(
-      new Request('http://localhost', { method: 'OPTIONS' }),
-    )
-    expect(result.status).toEqual(200)
-    expect(result.headers.get('access-control-allow-methods')).toEqual(
-      'GET, OPTIONS',
-    )
-  })
+test('handle OPTIONS', async () => {
+  const env = getMiniflareBindings()
+  const result = await handleRequest(
+    new Request('http://localhost', { method: 'OPTIONS' }),
+    env,
+  )
+  expect(result.status).toEqual(200)
+  expect(result.headers.get('access-control-allow-methods')).toEqual(
+    'GET, OPTIONS',
+  )
 })
