355db9ca16e835553ab6896000a1d94b58469173
spencerwooo <spencer.woo@outlook.com>
2022-02-22 23:17:41 +0800

    route to url builder

--- docs/src/routes.tsx
+++ docs/src/routes.tsx
@@ -2,11 +2,16 @@ import type { PathRouteProps } from 'react-router-dom'
 
 import Construction from 'lib/pages/construction'
 import Home from 'lib/pages/home'
+import Builder from 'lib/pages/builder'
 
 export const routes: Array<PathRouteProps> = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/s/:source',
+    element: <Builder />,
   },
   {
     path: '/construction',
