db56f72a981e1cffbe6fa4bce1e3ca363c628e64
ccbikai <ccbikai@qq.com>
2023-12-30 17:11:34 +0800

    refactor(inoreader): remove content-type

--- worker/src/providers/inoreader.ts
+++ worker/src/providers/inoreader.ts
@@ -7,7 +7,6 @@ export default async function inoreaderProvider(
   try {
     const resp = await fetch(`https://www.innoreader.com/feed/${encodeURIComponent(key)}`, {
       headers: {
-        'content-type': 'application/x-www-form-urlencoded',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
       },
       cf: { cacheEverything: true },
